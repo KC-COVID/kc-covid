da5a2dad8cc51517e8be4038b28c6e5b5ef5bc09
Mark Smith <mrs3816@gmail.com>
2020-03-19 23:28:35 -0500

    Slight cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   },
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, '.')
   },
   ...relativePath,
 };
