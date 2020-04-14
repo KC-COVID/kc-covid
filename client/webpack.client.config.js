345884f4eefad10add0640b4c532a5837e6e4b5a
Andrew Wilson <bawilson1991@gmail.com>
2020-04-13 19:26:56 -0500

    Split Client and Add Express Server

---
+++ client/webpack.client.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.jsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(jsx|js)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    }, {
+      test: /\.(scss|css)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    }, {
+      test: /\.md$/,
+      use: 'raw-loader',
+    }, {
+      test: /\.(html|png|svg|jpg|gif|otf|eot|ttf|svg|woff|woff2)$/,
+      use: 'file-loader',
+    }],
+  },
+  resolve: { extensions: ['.js', '.jsx', '.json'] },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    hot: true,
+    historyApiFallback: true,
+  },
+};
